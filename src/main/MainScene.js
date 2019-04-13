851a95dab041e25dc53aeb221a1ba396d5246c3d
haveSE <46266835@qq.com>
2019-04-13 19:53:19 +0800

    add

--- src/main/MainScene.js
+++ src/main/MainScene.js
@@ -23,10 +23,13 @@ main.MainScene = cc.Scene.extend({
                 }
             },this);
         cc.eventManager.addListener(this._listener, this);
+
+        this._initUI();
     },
 
     _initUI : function () {
     	this.btn_time = this.mainLayer.getChildByName("btn_time");
+        this.btn_time.addTouchEventListener(this._handClick.bind(this),this.btn_time);
     	this.btn_putong = this.mainLayer.getChildByName("btn_putong");
     	this.btn_wuxian = this.mainLayer.getChildByName("btn_wuxian");
     	this.btn_shop = this.mainLayer.getChildByName("btn_shop");
@@ -34,5 +37,13 @@ main.MainScene = cc.Scene.extend({
     	
     	this.img_yuan = this.mainLayer.getChildByName("img_yuan");
     	this.img_xian = this.mainLayer.getChildByName("img_xian");
+    },
+
+    _handClick : function(btn) {
+        switch (btn) {
+            case this.btn_time :
+                sceneMgr.switchToGame(); 
+            break;
+        }
     }
 });
